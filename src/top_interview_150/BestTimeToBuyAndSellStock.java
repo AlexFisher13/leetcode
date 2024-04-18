3123e6011da48bd90ce084ce2275a546bd7315b2
fisher <fisheralexandr@gmail.com>
2024-04-18 16:03:53 +0300

    3st, 4st tasks

---
+++ src/top_interview_150/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,35 @@
+package top_interview_150;
+/**
+ * EASY
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/?envType=study-plan-v2&envId=top-interview-150
+ *
+ * Возьмем первое цену как минимальную цену покупки.
+ * Будем пробегать по всем ценам.
+ * Оносительно каждой новой цены вычислять потенциальный профит. И если он больше предыдущего профита, то обновлять его.
+ * Также будем искать меньшую цену покупки, и обновлять ее при нахождении. И дальше уже от нее искать профит.
+ * Если от нее профит не будет найден, то мы профит и не обновим.
+ */
+public class BestTimeToBuyAndSellStock {
+    public static void main(String[] args) {
+        int[] prices = new int[]{2, 4, 1};
+        int maxProfit = getMaxProfit(prices);
+        System.out.println(maxProfit);
+    }
+
+    private static int getMaxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            int currentPrice = prices[i];
+            int potentialProfit = currentPrice - minPrice;
+
+            maxProfit = Math.max(potentialProfit, maxProfit);
+            minPrice = Math.min(minPrice, currentPrice);
+        }
+
+
+        return maxProfit;
+    }
+}
---
+++ src/top_interview_150/two_pointers/RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,47 @@
+package top_interview_150.two_pointers;
+
+/**
+ * MEDIUM
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/?envType=study-plan-v2&envId=top-interview-150
+ *
+ * */
+public class RemoveDuplicatesFromSortedArrayII {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4, 5};
+        int k = removeDuplicates(nums);
+        System.out.println(nums);
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        // Проверяем, не является ли массив nums пустым или равным нулю
+        if (nums == null || nums.length == 0) {
+            return 0; // Если да, то возвращаем 0, так как нет дубликатов
+        }
+
+        int slow = 0; // Инициализируем медленный указатель
+        int count = 1; // Счётчик для отслеживания количества встреченных элементов
+
+        // Проходим по массиву начиная со второго элемента (fast = 1)
+        for (int fast = 1; fast < nums.length; fast++) {
+            // Если текущий элемент равен предыдущему
+            if (nums[fast] == nums[slow]) {
+                count++; // Увеличиваем счётчик
+                // Если количество не превышает 2, то мы можем добавить элемент
+                // к результату и сместить медленный указатель
+                if (count <= 2) {
+                    slow++; // Смещаем медленный указатель
+                    nums[slow] = nums[fast]; // Записываем текущий элемент
+                }
+            } else {
+                // Если текущий элемент отличается от предыдущего
+                slow++; // Смещаем медленный указатель
+                nums[slow] = nums[fast]; // Записываем текущий элемент
+                count = 1; // Сбрасываем счётчик
+            }
+        }
+
+        // Возвращаем количество уникальных элементов в массиве (slow + 1)
+        return slow + 1;
+    }
+}
